4ebbebeb02cac8eae6328f76e595d60e8cb28084
Stefen Gerrit Pegels <sgp62@cornell.edu>
2022-02-21 12:21:54 -0500

    debugging lab 2

--- lab2/lab2_column_testbench.v
+++ lab2/lab2_column_testbench.v
@@ -8,15 +8,15 @@ module testbench();
 	
 	reg clk_50, clk_25, reset;
 	
-	reg [31:0] index;
+	reg          [31:0] index;
 
-	reg  signed [17:0]  initial_u_n;
-	reg  signed [17:0]  initial_u_n_prev;
-	reg  signed [17:0]  rho;
-	reg  signed [17:0]  g_tension;
-	reg  signed [17:0]  eta_term;
+	reg  signed  [17:0]  initial_u_n;
+	reg  signed  [17:0]  initial_u_n_prev;
+	reg  signed  [17:0]  rho;
+	reg  signed  [17:0]  g_tension;
+	reg  signed  [17:0]  eta_term;
 
-	wire  signed [17:0] out;
+	wire signed  [17:0] out;
 	
 	wire 	     [17:0] m10k_read_data;
 	reg			 [17:0] m10k_data_buffer, m10k_write_data;
@@ -29,10 +29,14 @@ module testbench();
 	reg 				m10k_prev_write_en;
 
 
-	reg [4:0]    state;
-	reg [4:0]    init_state;
-	reg [8:0]    init_addr ;
-	reg 		memory_init_en;
+	reg          [4:0]    state;
+	reg          [4:0]    init_state;
+	reg          [8:0]    init_addr ;
+	reg 		          memory_init_en;
+	reg			 [4:0]    column_idx;
+	reg          [17:0]   u_n_down_reg;
+	reg          [17:0]   u_n_reg;
+	reg			 [31:0]   column_size;
 	
 	//Initialize constants
 	initial begin
@@ -42,10 +46,15 @@ module testbench();
 		eta_term         = 18'h0000d;
 		g_tension        = 18'h02000;
 		
+		state = 5'd0;
+		column_size      = 9'd30;
 		memory_init_en = 1'b1;
 		init_addr = 9'd0;
-		m10k_write_en = 1'b1;
-		m10k_prev_write_en = 1'b1;
+		m10k_write_en = 1'b0;
+		m10k_prev_write_en = 1'b0;
+		column_idx = 9'd1;
+		u_n_down_reg = 18'h0;
+		u_n_reg = 18'h1ffff; //TODO: Resolve hard coding (Maybe take parameter inputs)
 	end
 
 	
@@ -89,13 +98,12 @@ module testbench();
 	always @ (posedge clk_50) begin //Memory init
 		if(memory_init_en) begin
 			m10k_write_addr <= init_addr;
-			m10k_write_data <= 18'ha;
-			
+			m10k_write_data <= (init_addr < (column_size >> 1)) ? init_addr : (column_size-1) - init_addr; //TODO: Resolve hard coding for variable column lengths
 			
 			m10k_prev_write_addr <= init_addr;
-			m10k_prev_write_data <= 18'ha;
+			m10k_prev_write_data <= (init_addr < (column_size >> 1)) ? init_addr : (column_size-1) - init_addr;
 			
-			if(init_addr == 9'd511) begin
+			if(init_addr == (column_size-1)) begin
 				memory_init_en <= 1'b0;
 				m10k_prev_write_en <= 1'b0;
 				m10k_write_en <= 1'b0;
@@ -104,32 +112,59 @@ module testbench();
 			
 		end
 	end
-	
-	always @ (posedge clk) begin
-		if(state == 5'd0)begin
-			//m10k_read_addr <= 9'd42;
-			m10k_write_addr <= (memory_init_en) ? 18'h0 ;
+	always @ (posedge clk_50) begin
+		if(~memory_init_en) begin
+			if(state == 5'd0)begin
+				m10k_read_addr <= column_idx+1;
+				state <= 5'd1;
+			end
 			
-			m10k_write_data <= 18'h1ffff;
-			m10k_write_en <= 1'b1;
-			state <= 5'd1;
-		end
-		
-		if(state == 5'd1)begin
-			m10k_read_addr <= 9'd42;
-			m10k_write_en <= 1'b0;
-			state <= 5'd2;
-		end
-		
-		if(state == 5'd2)begin
-			state <= 5'd3;
-		end
-		
-		if(state == 5'd3)begin
-			m10k_data_buffer <= m10k_read_data;
-			state <= 5'd0;
+			if(state == 5'd1)begin
+				state <= 5'd2;
+			end
+			
+			if(state == 5'd2)begin
+				state <= 5'd0;
+				column_idx = (column_idx == (column_size-2)) ? 1 : column_idx + 1;
+			end
 		end
 	end
+	
+	assign out = m10k_read_data;
+	
+	/* always @ (posedge clk_50) begin
+		if(~memory_init_en) begin
+			if(state == 5'd0)begin
+				m10k_read_addr <= column_idx+1;
+				m10k_prev_read_addr <= column_idx;
+				m10k_write_en <= 1'b0;
+				m10k_prev_write_en <= 1'b0;
+				state <= 5'd1;
+			end
+			
+			if(state == 5'd1)begin
+				state <= 5'd2;
+			end
+			
+			if(state == 5'd2)begin
+				m10k_write_en <= 1'b1;
+				m10k_prev_write_en <= 1'b1;
+				
+				m10k_write_addr <= column_idx;
+				m10k_prev_write_addr <= column_idx;
+				
+				m10k_prev_write_data <= u_n_reg;
+				u_n_down_reg <= u_n_reg;
+				
+				u_n_reg <= m10k_read_data;
+				m10k_write_data <= out;
+				
+				state <= 5'd0;
+				column_idx = (column_idx == (column_size-2)) ? 1 : column_idx + 1;
+			end
+		end
+	end
+	*/
 
 	
 
@@ -167,6 +202,17 @@ M10K_512_18 u_n_prev_m10k (
 	.rdaddress (m10k_prev_read_addr)
 );
 
+/* column_node calc_module (
+	.out       (out),
+	.rho       (rho),
+	.g_tension (g_tension),
+	.eta_term  (eta_term),
+	.u_n       (u_n_reg),
+	.u_n_prev  (m10k_prev_read_data),
+	.u_n_up    (m10k_read_data),
+	.u_n_down  (u_n_down_reg)
+);
+ */
 
 //single_node lab2_single (clk_50, reset, out, initial_u_n, initial_u_n_prev, rho, g_tension, eta_term, temp1_trace, temp2_trace);
 	
--- lab2/lab2_node_column.v
+++ lab2/lab2_node_column.v
@@ -18,24 +18,44 @@ module M10K_512_18(
 			mem[wraddress] <= data;
 	end
 	always @ (posedge clock) begin
-		q <= mem[read_address_reg];
 		read_address_reg <= rdaddress;
+		q <= mem[read_address_reg]; // Remember to change because not synthesizable
 	end
 endmodule 
 
-module node_column( 
-    output reg [31:0] q,
-    input [31:0] d,
-    input [7:0] write_address, read_address,
-    input we, clk
+module column_node( 
+  input  signed   [17:0] rho, g_tension, eta_term, u_n, u_n_prev, u_n_up, u_n_down,
+  output signed  [17:0] out
 );
-	 // force M10K ram style
-    reg [31:0] mem [255:0]  /* synthesis ramstyle = "no_rw_check, M10K" */;
-	 
-    always @ (posedge clk) begin
-        if (we) begin
-            mem[write_address] <= d;
-        end
-        q <= mem[read_address]; // q doesn't get d in this clock cycle
-    end
+  
+  wire   signed  [17:0] u_n_next, u_cent;
+  wire  signed   [17:0] rho_eff;
+  wire  signed   [17:0] temp1;
+  wire  signed   [17:0] temp2;
+  wire  signed   [17:0] u_cent_g_tension;
+  
+/*   always @ (posedge clk) 
+  begin
+    if (reset==1) begin//reset 
+      u_n <= initial_u_n ; // 
+	  u_n_prev <= initial_u_n_prev;
+	  //u_n_next <= 18'h0;
+    end 
+	else begin
+      u_n_prev <= u_n; 
+      u_n <= u_n_next; 
+	end
+  end */
+  
+
+  //assign rho_eff = (0.49 < (rho + u_cent_g_tension)) ? 0.49:(rho + u_cent_g_tension);
+  assign u_cent = u_n;
+  assign out = u_n;
+  //signed_mult mult4 (.out(u_cent_g_tension), .a(u_cent), .b(g_tension));
+  
+  //u_n_next = (1-eta_term) * [rho *(-4*u_n) + 2*u_n - (1-eta_term)*u_n_prev
+  signed_mult mult1 (.out(temp1), .a(rho), .b(u_n_up + u_n_down - u_n << 2));
+  signed_mult mult2 (.out(temp2), .a(18'h1ffff-eta_term), .b(u_n_prev));
+  signed_mult mult3 (.out(u_n_next), .a((u_n << 1) + temp1 - temp2), .b(18'h1ffff-eta_term));
+  
 endmodule
